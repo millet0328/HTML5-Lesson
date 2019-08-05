b21d6e34acea4be99bc39c9e945c0b4bf2062c41
silky <nn880328@126.com>
2019-08-05 18:51:53 +0800

    上课

--- 2-JavaScript/lesson/1-变量-运算符-数据类型/js/test.js
+++ 2-JavaScript/lesson/1-变量-运算符-数据类型/js/test.js
@@ -1,8 +1,6 @@
 // 查找元素
-// 匈牙利命名
-var oBtn = document.getElementById("btn");
-// 驼峰式命名,多个单词首字母大写
-// 绑定事件
+var oBtn = document.getElementById('btn');
+// 绑定click事件
 oBtn.onclick = function() {
-	alert("你好，北京");
+	console.log("HELLO WORLD!");
 }
