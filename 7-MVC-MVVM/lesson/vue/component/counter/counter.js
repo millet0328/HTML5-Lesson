f6a4ce43301770356fff073180bb94f6f929ded1
鞠祥明 <nn880328@126.com>
2020-06-05 09:21:02 +0800

    更新教材

--- 7-MVC-MVVM/lesson/vue/component/counter/counter.js
+++ 7-MVC-MVVM/lesson/vue/component/counter/counter.js
@@ -1,20 +1,17 @@
 Vue.component('counter', {
-	data: function() {
-		return {
-			count: 1
-		}
-	},
+	props: ['num'],
 	template: `
-				<div class="counter-box">
-					<button class="btn left" :disabled="count==1" @click="reduceHandle" type="button">-</button>
-					<input class="num" type="text" v-model="count">
-					<button class="btn right" @click="count++" type="button">+</button>
-				</div>`,
+	<div class="counter-box">
+		<button class="btn left" :disabled="num==1" @click="handleDecreass" type="button">-</button>
+		<input class="num" type="text" v-model="num">
+		<button class="btn right" @click="handleIncreass" type="button">+</button>
+	</div>`,
 	methods: {
-		reduceHandle: function() {
-			if (this.count > 1) {
-				this.count--;
-			}
+		handleIncreass() {
+			this.$emit('update:num', this.num + 1)
+		},
+		handleDecreass() {
+			this.$emit('update:num', this.num - 1)
 		}
 	}
 });
